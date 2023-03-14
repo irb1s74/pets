c98264626439f5342d7381f64f2e9f8c7e53ab7a
irb1s <kolyirb1s@gmail.com>
2023-03-14 15:10:40 +0500

    Commit

--- config/webpack/buildPlugins.ts
+++ config/webpack/buildPlugins.ts
@@ -1,6 +1,7 @@
 import { DefinePlugin, WebpackPluginInstance } from 'webpack'
 import { BuildOptions } from './buildTypes'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInstance[] => {
   const plugins: WebpackPluginInstance[] = [
@@ -11,8 +12,16 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions): WebpackPluginInsta
       __IS_DEV__: JSON.stringify(isDev),
     }),
   ]
+
   if (isDev) {
     console.log('dev')
+  }
+
+  if (!isDev) {
+    plugins.push(new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }))
   }
   return plugins
 }
