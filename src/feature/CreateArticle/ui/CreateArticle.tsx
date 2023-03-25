bc24f0889ca2d13f68930184aaa5c0b974cfa38d
irb1s <kolyirb1s@gmail.com>
2023-03-25 11:36:15 +0500

    Commit

--- src/feature/CreateArticle/ui/CreateArticle.tsx
+++ src/feature/CreateArticle/ui/CreateArticle.tsx
@@ -1,10 +1,11 @@
-import { memo } from 'react'
+import { createRef, memo, useState } from 'react'
 import { Avatar } from 'shared/ui/Avatar'
 import { useSelector } from 'react-redux'
 import { getUserAuthData } from 'entities/User'
 import Photo from 'shared/assets/icons/photo.svg'
 import classNames from 'classnames'
 import styles from './CreateArticle.module.scss'
+import { AppImage } from 'shared/ui/AppImage'
 
 interface CreateArticleProps {
   className?: string
@@ -13,16 +14,43 @@ interface CreateArticleProps {
 export const CreateArticle = memo((props: CreateArticleProps) => {
   const { className } = props
   const user = useSelector(getUserAuthData)
+  const filesInput = createRef<HTMLInputElement>()
+  const [previewImages, setPreviewImages] = useState<string[]>()
+  const handleUpdateFiles = async () => {
+    if (filesInput.current.files && filesInput.current.files.length) {
+      const images = Array.from(filesInput.current.files).map((file) => URL.createObjectURL(file))
+      if (images.length) {
+        setPreviewImages(images)
+      }
+    }
+  }
+  const handleSelectFiles = () => {
+    filesInput.current.click()
+  }
   return (
     <div className={classNames(styles.CreateArticle, {}, [className])}>
       <Avatar src={user.avatar} alt={user.username} size={32} />
       <div className={styles.CreateArticle__content}>
-        <textarea placeholder='Что у вас нового?' />
+        <textarea rows={4} placeholder='Что у вас нового?' />
       </div>
       <div className={styles.CreateArticle__images}>
-        <div className={styles.icon}>
-          <Photo />
-        </div>
+        {previewImages ? (
+          <>
+            {previewImages.map((img, index) => (
+              <div className={styles.img} key={index}>
+                <AppImage src={img} />
+              </div>
+            ))}
+            <div onClick={handleSelectFiles} className={styles.btn}>
+              +
+            </div>
+          </>
+        ) : (
+          <div onClick={handleSelectFiles} className={styles.icon}>
+            <Photo />
+          </div>
+        )}
+        <input ref={filesInput} onChange={handleUpdateFiles} type='file' multiple hidden />
       </div>
     </div>
   )
